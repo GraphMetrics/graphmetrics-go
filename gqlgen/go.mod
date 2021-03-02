976911c4c19d7ef819951311769dc4f707e62eeb
Emile Fugulin <code@efugulin.com>
2021-03-02 11:35:17 -0500

    [GQLGEN] Add support for operations

--- gqlgen/go.mod
+++ gqlgen/go.mod
@@ -4,5 +4,7 @@ go 1.15
 
 require (
 	github.com/99designs/gqlgen v0.13.0
-	github.com/graphmetrics/graphmetrics-go v0.2.3
+	github.com/graphmetrics/graphmetrics-go v0.3.0
+	github.com/graphmetrics/logger-go v0.2.1
+	github.com/vektah/gqlparser/v2 v2.1.0
 )
--- gqlgen/handler.go
+++ gqlgen/handler.go
@@ -5,8 +5,12 @@ import (
 	"time"
 
 	"github.com/99designs/gqlgen/graphql"
+	"github.com/graphmetrics/logger-go"
+	"github.com/vektah/gqlparser/v2/ast"
+
 	"github.com/graphmetrics/graphmetrics-go"
 	"github.com/graphmetrics/graphmetrics-go/client"
+	"github.com/graphmetrics/graphmetrics-go/signature"
 )
 
 type Extension interface {
@@ -18,54 +22,81 @@ type Extension interface {
 }
 
 func NewExtension(cfg *graphmetrics.Configuration) Extension {
-	clientExt := cfg.ClientExtractor
-	if clientExt == nil {
-		clientExt = func(context.Context) client.Details {
-			return client.Details{}
-		}
-	}
 	agg := graphmetrics.NewAggregator(cfg)
 	go agg.Start()
 	return &extensionImpl{
-		agg:       agg,
-		clientExt: clientExt,
+		aggregator:      agg,
+		clientExtractor: cfg.GetClientExtractor(),
+		logger:          cfg.GetLogger(),
 	}
 }
 
 type extensionImpl struct {
-	agg       *graphmetrics.Aggregator
-	clientExt client.Extractor
+	aggregator      *graphmetrics.Aggregator
+	clientExtractor client.Extractor
+	schema          *ast.Schema
+
+	logger logger.Logger
 }
 
 func (*extensionImpl) ExtensionName() string {
 	return "GraphMetricsExtension"
 }
 
-func (*extensionImpl) Validate(_ graphql.ExecutableSchema) error {
+func (e *extensionImpl) Validate(schema graphql.ExecutableSchema) error {
+	e.schema = schema.Schema()
 	return nil
 }
 
 func (e *extensionImpl) InterceptOperation(ctx context.Context, next graphql.OperationHandler) graphql.ResponseHandler {
-	// NOT IMPLEMENTED YET
-	return next(ctx)
+	operation := graphql.GetOperationContext(ctx)
+	caller := e.clientExtractor(ctx)
+	sign, err := signature.OperationSignature(e.schema, operation.RawQuery, operation.OperationName)
+	if err != nil {
+		e.logger.Error("unable to build operation signature", map[string]interface{}{
+			"err":       err,
+			"operation": operation.OperationName,
+		})
+	}
+	hash := signature.OperationHash(sign)
+
+	handler := next(ctx)
+	return func(ctx context.Context) *graphql.Response {
+		res := handler(ctx)
+		duration := time.Since(operation.Stats.OperationStart)
+		e.aggregator.PushOperation(&graphmetrics.OperationMessage{
+			Name:      operation.OperationName,
+			Type:      string(operation.Operation.Operation),
+			Hash:      hash,
+			Signature: sign,
+			HasErrors: len(res.Errors) > 0,
+			Duration:  duration,
+			Client:    caller,
+		})
+
+		return res
+	}
 }
 
 func (e *extensionImpl) InterceptField(ctx context.Context, next graphql.Resolver) (res interface{}, err error) {
-	field := graphql.GetFieldContext(ctx)
 	start := time.Now()
+
+	field := graphql.GetFieldContext(ctx)
+	caller := e.clientExtractor(ctx)
 	res, err = next(ctx)
 	duration := time.Since(start)
-	e.agg.PushField(&graphmetrics.FieldMessage{
+	e.aggregator.PushField(&graphmetrics.FieldMessage{
 		TypeName:   field.Object,
 		FieldName:  field.Field.Name,
 		ReturnType: field.Field.Definition.Type.String(),
 		Error:      err,
 		Duration:   duration,
-		Client:     e.clientExt(ctx),
+		Client:     caller,
 	})
+
 	return res, err
 }
 
 func (e *extensionImpl) Close() error {
-	return e.agg.Stop()
+	return e.aggregator.Stop()
 }
